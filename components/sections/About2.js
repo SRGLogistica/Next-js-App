0ef6d454b35a402af4fb44f2d113e8430205dad7
srgsolutioncenter@gmail.com <seu@email.com>
2025-05-27 10:37:05 +0200

    About2

--- components/sections/About2.js
+++ components/sections/About2.js
@@ -14,16 +14,16 @@ const About2 = () => {
                     <div className="content-column col-xl-4 col-lg-6 col-md-12 col-sm-12 wow fadeInLeft">
                     <div className="inner-column">
                         <div className="sec-title">
-                        <span className="sub-title">Get to know us</span>
-                        <h2 className="scrub-each-word text-split">Moving products across borders</h2>
+                        <span className="sub-title">Conheça-nos</span>
+                        <h2 className="scrub-each-word text-split">Transporte de produtos através de fronteiras</h2>
                         </div>
-                        <h5 className="title">Create Opportunity to Reach Potential</h5>
-                        <div className="text">There are many variations of passages of lorem Ipsum available majority have suffered ipsum even slightly believable.</div>
+                        <h5 className="title">Crie oportunidades para atingir o potencial</h5>
+                        <div className="text">Existem muitas variações de passagens do lorem Ipsum disponíveis, a maioria sofreu ipsum, mesmo ligeiramente crível.</div>
                         <div className="icon-box">
                         <i className="icon flaticon-delivery-courier"></i>
-                        <h5 className="title">We Provide Full Rang of Logistics Transportation Worldwide</h5>
+                        <h5 className="title">Oferecemos uma gama completa de transporte logístico em todo o mundo</h5>
                         </div>
-                        <Link href="page-about" className="theme-btn btn-style-one"><span className="btn-title">Discover More</span></Link>
+                        <Link href="page-about" className="theme-btn btn-style-one"><span className="btn-title">Descubra mais</span></Link>
                     </div>
                     </div>
 
@@ -34,8 +34,8 @@ const About2 = () => {
                         <div className="exp-box">
                         <div className="icon-building-2"></div>
                         <i className="icon flaticon-logistics-2"></i>
-                        <span className="count">30</span>
-                        <div className="text">Years of Cleaning <br />experience</div>
+                        <span className="count">5</span>
+                        <div className="text">Anos de experiência  <br />em logistica</div>
                         </div>
                     </div>
                     </div>
@@ -48,7 +48,7 @@ const About2 = () => {
                             <i className="icon flaticon-order"></i>
                             <div className="content">
                                 <div className="counter-one"><CounterUp count={478} time={3} /></div>
-                                <div className="counter-text">Projects Completed</div>
+                                <div className="counter-text">Projetos concluídos</div>
                             </div>
                             </div>
                         </div>
@@ -58,7 +58,7 @@ const About2 = () => {
                             <i className="icon flaticon-delivery-man"></i>
                             <div className="content">
                                 <div className="counter-one"><CounterUp count={650} time={3} /></div>
-                                <div className="counter-text">Happy Customers</div>
+                                <div className="counter-text">Clientes satisfeitos</div>
                             </div>
                             </div>
                         </div>
@@ -68,7 +68,7 @@ const About2 = () => {
                             <i className="icon flaticon-logistic"></i>
                             <div className="content">
                                 <div className="counter-one"><CounterUp count={900} time={3} /></div>
-                                <div className="counter-text">Delivered in Time</div>
+                                <div className="counter-text">Entregue no prazo</div>
                             </div>
                             </div>
                         </div>
