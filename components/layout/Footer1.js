ac3a3c3866eaa041571a8d689462b1f8d4270b67
srgsolutioncenter@gmail.com <seu@email.com>
2025-05-30 11:36:18 +0200

    adicionando o slogan da SRG

--- components/layout/Footer1.js
+++ components/layout/Footer1.js
@@ -33,7 +33,7 @@ export default function Footer1() {
 					<div className="footer-column col-lg-3 col-md-6 col-sm-12">
 						<div className="footer-widget about-widget">
 						<div className="logo"><Link href="#"><img src="images/logo.png" alt="Logo"/></Link></div>
-						<div className="text">Trabalhamos com paixão por assumir desafios e criar novos no setor de publicidade.</div>
+						<div className="text">Conectando Você ao Melhor em Tecnologia de Qualidade!</div>
 						<Link href="page-about" className="theme-btn btn-style-one"><span className="btn-title">Sobre nós</span></Link>
 						</div>
 					</div>
